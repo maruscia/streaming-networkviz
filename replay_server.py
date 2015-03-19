85b018592b1d455ab5d7352bdd8000e9b3793086
Mara Sorella <maruscia@maruscia.it>
2015-03-19 22:07:02 +0100

    added: - edge weight support - time warping - early adopters highlighting

--- replay_server.py
+++ replay_server.py
@@ -47,12 +47,12 @@ import urllib.parse
 import tweepy
 import re
 try:
-    import simplejson
+	import simplejson
 except ImportError:
-    try:
-        import json as simplejson
-    except:
-        raise "Requires either simplejson or Python 2.6!"
+	try:
+		import json as simplejson
+	except:
+		raise "Requires either simplejson or Python 2.6!"
 import threading
 import queue
 import socket
@@ -65,162 +65,189 @@ active_queues = []
 
 class StreamingListener(tweepy.StreamListener):
 
-    def __init__(self, timewarp, *args, **kwargs):
-        tweepy.StreamListener.__init__(self, *args, **kwargs)
-        self.known_users = {}
-        self.before = None
-        self.timewarp = timewarp
+	def __init__(self, timewarp, *args, **kwargs):
+		tweepy.StreamListener.__init__(self, *args, **kwargs)
+		self.known_users = {}
+		self.before = None
+		self.timewarp = timewarp
 
-    def on_status(self, status):
-        #print(status.text)
+	def on_status(self, status):
+		#print(status.text)
 
-        date = status.created_at
-        if not self.before:
-            self.before = date
+		date = status.created_at
+		if not self.before:
+			self.before = date
 
-        if date > self.before:
-            diff = date - self.before
+		if date > self.before:
+			diff = date - self.before
 
-            sleeptime = diff.seconds + diff.microseconds*10e-6
-            if sleeptime > 0:
-                time.sleep(sleeptime*self.timewarp)
-            self.before = date
+			sleeptime = diff.seconds + diff.microseconds*10e-6
+			if sleeptime > 0:
+				time.sleep(sleeptime*self.timewarp)
+			self.before = date
 
-        m = re.search('(?<=RT\s@)\w+', status.text)
-        if m:
-            source_user = m.group(0).lower()
-            target_user = status.user.screen_name
-            id = status.id
-            text = status.text
+		m = re.search('(?<=RT\s@)\w+', status.text)
+		if m:
+			source_user = m.group(0).lower()
+			target_user = status.user.screen_name
+			id = status.id
+			text = status.text
 
-            dispatch_event((id, source_user, target_user, text, date))
+			dispatch_event((id, source_user, target_user, text, date))
 
 def dispatch_event(e):
-    for q in active_queues:
-        q.put(e)
+	for q in active_queues:
+		q.put(e)
 
 class RequestHandler(BaseHTTPRequestHandler):
 
-    def __init__(self, *args, **kwargs):
-        BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
 
-    def do_POST(self):
-        pass
+	def handle_tweets(self,terms):
+		while True:
+			(id, source, target, text, date) = self.queue.get()
+			if id == None:
+				break
 
-    def do_GET(self):
+			found = False
+			for term in terms:
+				if re.search(term, text.lower()):
+					found = True
+			if not found:
+				continue
 
-        param_str = urllib.parse.urlparse(self.path).query
-        parameters = urllib.parse.parse_qs(param_str, keep_blank_values=False)
-        if "q" not in parameters:
-            return
 
-        q = parameters["q"][0]
-        terms = q.split(",")
+			r=84./255.
+			g=148./255.
+			b=183./255.
+			size = 5
 
-        print("Request for tweets, query '%s'"%q)
+			try:
 
-        self.queue = queue.Queue()
-        self.known_users = {}
-        active_queues.append(self.queue)
+				if source not in self.known_users:
+					if len(self.known_users) == 0: #first user
+						size=15
+					if len(self.known_users)<10:
+						r=255./255.
+						g=0.0
+						b=0.0
+					self.known_users[source] = source
+					event = simplejson.dumps({'an':{source:{'label':source, 'size':size, 'r':r, 'g':g, 'b':b}}})
+					self.wfile.write(str.encode(event))
+					self.wfile.write(str.encode('\r\n\r\n'))
 
-        self.wfile.write(str.encode('\r\n'))
+				if target not in self.known_users:
+					size =5
+					self.known_users[target] = target
+					event = simplejson.dumps({'an':{target:{'label':target, 'size':size, 'r':r, 'g':g, 'b':b}}})
+					self.wfile.write(str.encode(event))
+					self.wfile.write(str.encode('\r\n'))
 
-        while True:
-			#self.handleTweet()
-            (id, source, target, text, date) = self.queue.get()
-            if id == None:
-                break
 
-            found = False
-            for term in terms:
-                if re.search(term, text.lower()):
-                    found = True
-            if not found:
-                continue
+				id=source+"__"+target
 
-            try:
+				try:
+					w = self.seen_edges[id]+1.0
+					event = simplejson.dumps({'ce':{id:{'weight':w}}})
+				except KeyError:
+					w=1.0
+					event = simplejson.dumps({'ae':{id:{'source':source, 'target':target, 'directed':True, 'weight':w, 'date':str(date)}}})
 
-                if source not in self.known_users:
-                    self.known_users[source] = source
-                    event = simplejson.dumps({'an':{source:{'label':source, 'size':5, 'r':84./255., 'g':148./255., 'b':183./255.}}})
-                    self.wfile.write(str.encode(event))
-                    self.wfile.write(str.encode('\r\n\r\n'))
+				self.seen_edges[id]=w
+				#print(w)
 
-                if target not in self.known_users:
-                    self.known_users[target] = target
-                    event = simplejson.dumps({'an':{target:{'label':target, 'size':5, 'r':84./255., 'g':148./255., 'b':183./255.}}})
-                    self.wfile.write(str.encode(event))
-                    self.wfile.write(str.encode('\r\n'))
+				self.wfile.write(str.encode(event))
+				self.wfile.write(str.encode('\r\n'))
 
-                event = simplejson.dumps({'ae':{id:{'source':source, 'target':target, 'directed':True, 'weight':2.0, 'date':str(date)}}})
-                self.wfile.write(str.encode(event))
-                self.wfile.write(str.encode('\r\n'))
+			except socket.error:
+				print("Connection closed")
 
-            except socket.error:
-                print("Connection closed")
-                active_queues.remove(self.queue)
-                return
+	def do_POST(self):
+		pass
+
+	def do_GET(self):
+
+		param_str = urllib.parse.urlparse(self.path).query
+		parameters = urllib.parse.parse_qs(param_str, keep_blank_values=False)
+		if "q" not in parameters:
+			return
+
+		q = parameters["q"][0]
+		terms = q.split(",")
+
+		print("Request for tweets, query '%s'"%q)
+
+		self.queue = queue.Queue()
+		self.known_users = {}
+		self.seen_edges = {}
+		active_queues.append(self.queue)
+
+		self.wfile.write(str.encode('\r\n'))
+		self.handle_tweets(terms)
+		active_queues.remove(self.queue)
+		return
 
 class Player(threading.Thread):
-    def __init__(self, options, server):
-        self.options = options
-        self.server = server
-        threading.Thread.__init__(self)
+	def __init__(self, options, server):
+		self.options = options
+		self.server = server
+		threading.Thread.__init__(self)
 
-    def run(self):
-        print("Waiting %s seconds before start streaming" % self.options.delay)
-        time.sleep(self.options.delay)
+	def run(self):
+		print("Waiting %s seconds before start streaming" % self.options.delay)
+		time.sleep(self.options.delay)
 
-        print("Streaming retweets for file '%s'"%self.options.log)
-        listener = StreamingListener(self.options.timewarp)
-        f = open(self.options.log)
+		print("Streaming retweets for file '%s'"%self.options.log)
+		listener = StreamingListener(self.options.timewarp)
+		f = open(self.options.log)
 
-        line = f.readline()
-        while line != '':
-            listener.on_data(line)
-            line = f.readline()
+		line = f.readline()
+		while line != '':
+			listener.on_data(line)
+			line = f.readline()
 
-        print("Stream finished")
-        dispatch_event((None, None, None, None, None))
-        self.server.shutdown()
+		print("Stream finished")
+		dispatch_event((None, None, None, None, None))
+		self.server.shutdown()
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    """Handle requests in a separate thread."""
+	"""Handle requests in a separate thread."""
 
-    def start(self):
-        self.serve_forever()
+	def start(self):
+		self.serve_forever()
 
-    def stop(self):
-        self.socket.close()
+	def stop(self):
+		self.socket.close()
 
 def parseOptions():
-    parser = optparse.OptionParser()
-    parser.add_option("-l", "--log", type="string", dest="log", help="Log file of collected streaming data", default='undefined')
-    parser.add_option("-t", "--timewarp", type="float", dest="timewarp", help="Time warping factor, used to accelerate or slow down the replay", default='1.0')
-    parser.add_option("-d", "--delay", type="int", dest="delay", help="Starting delay in seconds", default='0')
-    parser.add_option("-s", "--serverport", type="int", dest="serverport", help="HTTP server port", default=8181)
-    (options, _) = parser.parse_args()
-    if options.log == 'undefined':
-        parser.error("Log file is mandatory")
-    return options
+	parser = optparse.OptionParser()
+	parser.add_option("-l", "--log", type="string", dest="log", help="Log file of collected streaming data", default='undefined')
+	parser.add_option("-t", "--timewarp", type="float", dest="timewarp", help="Time warping factor, used to accelerate or slow down the replay", default='1.0')
+	parser.add_option("-d", "--delay", type="int", dest="delay", help="Starting delay in seconds", default='0')
+	parser.add_option("-s", "--serverport", type="int", dest="serverport", help="HTTP server port", default=8181)
+	(options, _) = parser.parse_args()
+	if options.log == 'undefined':
+		parser.error("Log file is mandatory")
+	return options
 
 def main():
-    options = parseOptions()
+	options = parseOptions()
 
-    try:
-        server = ThreadedHTTPServer(('', options.serverport), RequestHandler)
+	try:
+		server = ThreadedHTTPServer(('', options.serverport), RequestHandler)
 
-        player = Player(options, server)
-        player.setDaemon(True)
-        player.start()
+		player = Player(options, server)
+		player.setDaemon(True)
+		player.start()
 
-        print('Test server running...')
-        server.start()
-    except KeyboardInterrupt:
-        print('Stopping server...')
-        server.stop()
-        dispatch_event((None, None, None, None, None))
-        sys.exit(0)
+		print('Test server running...')
+		server.start()
+	except KeyboardInterrupt:
+		print('Stopping server...')
+		server.stop()
+		dispatch_event((None, None, None, None, None))
+		sys.exit(0)
 
 if __name__ == '__main__':
-    main()
+	main()
